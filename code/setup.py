46cf64cade9b7b01fd78a22201561a3dbc248ae7
Jim Bridgewater <jwbwater@gmail.com>
2015-09-17 18:14:59 -0500

    Added cythonize back to setup.py
    to generate model.c and kept Extension to avoid the numpy/arrayobject.h
    not found error.

--- code/setup.py
+++ code/setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup, Extension
+from Cython.Build import cythonize
 import numpy
+
+cythonize("model.pyx")
 
 setup(
     ext_modules=[
-        Extension("model", ["model.c"],
-                  include_dirs=[numpy.get_include()]),
-    ],
+        Extension("model", ["model.c"], include_dirs=[numpy.get_include()])
+    ]
 )
+
 
